0b1720292afcb6ddf27b3c110c3c11256b61158e
xxyizumi <y.izumi@xendou.co.jp>
2026-02-15 16:30:30 +0900

    不要なファイルを削除

--- ‎mybatisBasics/task3/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (name, email) VALUES ('田中太郎', 'tanaka@example.com');
-INSERT INTO users (name, email) VALUES ('佐藤花子', 'sato@example.com');
-INSERT INTO users (name, email) VALUES ('鈴木一郎', 'suzuki@example.com');
